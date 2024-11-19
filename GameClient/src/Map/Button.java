39fa54eedc45273fdbfbcf134a15bf2470601098
Joohyun Lee <2271183@hansung.ac.kr>
2024-11-19 22:14:26 +0900

    Map 완성

---
+++ GameClient/src/Map/Button.java
@@ -0,0 +1,58 @@
+package Map;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Button {
+    public static final int BUTTON_WIDTH = 26;
+    public static final int BUTTON_HEIGHT = 9;
+
+    Image buttonImage = new ImageIcon("GameClient/image/map/button.png").getImage();
+    Rectangle rectButton;
+    int x, y, width, height;
+
+    public Button() {
+
+    }
+
+    public Button(int x, int y) {
+        this.x= x;
+        this.y= y;
+        this.width = BUTTON_WIDTH;
+        this.height = BUTTON_HEIGHT;
+        this.rectButton = new Rectangle(x,y,width, height);
+        setButtonImage();
+    }
+
+    public void setButtonImage() {
+        buttonImage = resizeImage(new ImageIcon("GameClient/image/map/button.png"),BUTTON_WIDTH, BUTTON_HEIGHT).getImage();
+    }
+
+    public ImageIcon resizeImage(ImageIcon icon, int width, int height) {
+        return new ImageIcon(icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
+    }
+
+    public Image getButtonImage() {
+        return buttonImage;
+    }
+
+    public Rectangle getRectButton() {
+        return rectButton;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ GameClient/src/Map/Item.java
@@ -0,0 +1,62 @@
+package Map;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Item {
+    public static final int ITEM_WIDTH = 25;
+    private int mapNumber;
+    Image itemImage;
+
+    int x, y, width, height;
+
+    public Item() {
+
+    }
+
+    public Item(int x, int y, int mapNumber) {
+        this.x= x;
+        this.y= y;
+        this.width = ITEM_WIDTH;
+        this.height = ITEM_WIDTH;
+        this.mapNumber = mapNumber;
+        setItemImage();
+    }
+
+    public void setItemImage() {
+        if(mapNumber%2 == 0) { //물고기
+            itemImage = resizeImage(new ImageIcon("GameClient/image/map/peng_item.png"), ITEM_WIDTH, ITEM_WIDTH).getImage();
+        }
+        else {
+            itemImage = resizeImage(new ImageIcon("GameClient/image/map/harp_item.png"), ITEM_WIDTH, ITEM_WIDTH).getImage();
+        }
+    }
+
+    public ImageIcon resizeImage(ImageIcon icon, int width, int height) {
+        return new ImageIcon(icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
+    }
+
+    public int getMapNumber() {
+        return mapNumber;
+    }
+
+    public Image getItemImage() {
+        return itemImage;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
