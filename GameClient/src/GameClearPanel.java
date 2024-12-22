75cbf53f1803d25ea1156f172f5e0a2a20998452
zuzihe <dya_yaaa@naver.com>
2024-12-22 21:39:32 +0900

    게임 클리어시  획득 아이템 개수와 별 표시

--- GameClient/src/GameClearPanel.java
+++ GameClient/src/GameClearPanel.java
@@ -10,6 +10,15 @@ public class GameClearPanel extends JPanel {
     private Image backgroundImage = new ImageIcon("GameClient/image/background/background_ingame_dark.png").getImage();
     private JLabel gameClearLabel = new JLabel(new ImageIcon("GameClient/image/game_clear.png"));
     private JLabel continueLabel = new JLabel(new ImageIcon("GameClient/image/continue.png"));
+    private JLabel fishLabel = new JLabel(new ImageIcon("GameClient/image/map/peng_item.png"));
+    private JLabel shellLabel = new JLabel(new ImageIcon("GameClient/image/map/harp_item.png"));
+
+    private JLabel starLabel1 = new JLabel();
+    private JLabel starLabel2 = new JLabel();
+    private JLabel starLabel3 = new JLabel();
+    private ImageIcon emptyStar = new ImageIcon("GameClient/image/star_empty.png");
+    private ImageIcon fullStar = new ImageIcon("GameClient/image/star_full.png");
+
     private ImageIcon yesImage = new ImageIcon("GameClient/image/yes.png");
     private ImageIcon noImage = new ImageIcon("GameClient/image/no.png");
     private ImageIcon yesHoverImage = new ImageIcon(applyColorFilter(yesImage, Color.decode("#B4FDFF")));
@@ -18,22 +27,48 @@ public class GameClearPanel extends JPanel {
     private JButton yesButton = new JButton(yesImage);
     private JButton noButton = new JButton(noImage);
 
+    private JLabel fishCountLabel = new JLabel();
+    private JLabel shellCountLabel = new JLabel();
+    private Font font_regular20 = new Font("Galmuri9 Regular", Font.PLAIN, 20);
 
-    public GameClearPanel(){
+    public GameClearPanel(int fishCount, int shellCount){
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setLayout(null);
         setVisible(true);
-        gameClearLabel.setBounds(239, 248, 347, 50);
-        continueLabel.setBounds(334, 333, 159, 18);
-        yesButton.setBounds(334, 369, 37, 15);
-        noButton.setBounds(457, 369, 31, 15);
+        starLabel1.setBounds(301, 143, 50, 50);
+        starLabel2.setBounds(389, 120, 50, 50);
+        starLabel3.setBounds(478, 143, 50, 50);
+        gameClearLabel.setBounds(239, 228, 347, 50);
+        fishLabel.setBounds(344, 298, 40, 40);
+        shellLabel.setBounds(344, 341, 40, 40);
+        fishCountLabel.setBounds(409, 311, 200, 20);
+        shellCountLabel.setBounds(409, 353, 200, 20);
+        continueLabel.setBounds(334, 405, 159, 18);
+        yesButton.setBounds(334, 441, 37, 15);
+        noButton.setBounds(457, 441, 31, 15);
 
         yesButton.setBorderPainted(false);
-        yesButton.setContentAreaFilled(false);  // 배경 없애기
+        yesButton.setContentAreaFilled(false);
         noButton.setBorderPainted(false);
-        noButton.setContentAreaFilled(false);  // 배경 없애기
+        noButton.setContentAreaFilled(false);
+
+        fishCountLabel.setText("X   " + fishCount);
+        shellCountLabel.setText("X   " + shellCount);
+        fishCountLabel.setFont(font_regular20);
+        fishCountLabel.setForeground(Color.WHITE);
+        shellCountLabel.setFont(font_regular20);
+        shellCountLabel.setForeground(Color.WHITE);
+
+        updateStars(fishCount, shellCount);
 
         add(gameClearLabel);
+        add(starLabel1);
+        add(starLabel2);
+        add(starLabel3);
+        add(fishLabel);
+        add(shellLabel);
+        add(fishCountLabel);
+        add(shellCountLabel);
         add(continueLabel);
         add(yesButton);
         add(noButton);
@@ -88,5 +123,24 @@ public class GameClearPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    // 별 개수를 업데이트하는 메서드
+    public void updateStars(int fishCount, int shellCount) {
+        int total = fishCount + shellCount;
+
+        if (total >= 10) {
+            starLabel1.setIcon(fullStar);
+            starLabel2.setIcon(fullStar);
+            starLabel3.setIcon(fullStar);
+        } else if (total >= 6) {
+            starLabel1.setIcon(fullStar);
+            starLabel2.setIcon(fullStar);
+            starLabel3.setIcon(emptyStar);
+        } else {
+            starLabel1.setIcon(fullStar);
+            starLabel2.setIcon(emptyStar);
+            starLabel3.setIcon(emptyStar);
+        }
     }
 }
